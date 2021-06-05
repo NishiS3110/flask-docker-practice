38d956a64a9175840597b3ee3b3aa1d93506bf90
Sotaro Nishimura <sotaro@SotaronoMacBook-Air.local>
2021-06-06 07:19:26 +0900

    Flask-docker

---
+++ app/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  values = { "name": "Taro"}
+  return render_template('index.html', data=values)
+
+@app.route("/test")
+def test():
+  values = { "message":"Hello! This is test page." }
+  return render_template('test.html', data=values)
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=80, debug=True)
